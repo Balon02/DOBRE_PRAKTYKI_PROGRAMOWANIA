53ee7ed25f883d44390189f748ade1203b5969a9
J.Balon <cooba.balon@gmail.com>
2026-01-16 15:46:49 +0100

    jit compilation functional

--- inference.py
+++ inference.py
@@ -39,11 +39,13 @@ class InferencePipeline:
 
         self._build_yolo()
         self._yolo.trainable=False
+        self._yolo.compile()
         self._yolo(ops.zeros((batch, 1024, 1024, 3), dtype='float16'))
 
         self._plate_cfg = load_plate_config_from_yaml('plate_config.yaml')
         self._ocr = load_keras_model('ocr.keras', self._plate_cfg)
         self._ocr.trainable=False
+        self._ocr.compile()
 
     def _build_yolo(self):
         base = YoloV11(depth=0.5, width=0.25, max_channels=1024, num_classes=1, input_res=1024, add_downsample=False)
